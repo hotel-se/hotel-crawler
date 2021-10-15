e1b912c4483d5802f6563165bf392306b28277b6
Edoardo Riggio <edo.riggio19@gmail.com>
2021-10-15 03:09:38 +0200

    fixed pagination (not really)

--- src/spiders/tripadvisor.py
+++ src/spiders/tripadvisor.py
@@ -10,16 +10,41 @@ class TripadvisorSpider(scrapy.Spider):
     'https://www.tripadvisor.com/Hotels-g188045-Switzerland-Hotels.html'
   ]
 
+
   def parse(self, response):
+    els = 0
+
     for hotel in response.css('.listItem'):
+      els += 1
+
       hotel_obj = Hotel()
 
       hotel_obj['url'] = f'https://tripadvisor.com/{str(hotel.css(".meta_listing::attr(data-url)").extract()[0]).strip()}'
       hotel_obj['name'] = str(hotel.css('.listing_title>a::text').extract()[0]).strip()
 
       yield scrapy.Request(response.urljoin(hotel_obj['url']),
-                           callback = self.parse_details,
+                           callback=self.parse_details,
                            cb_kwargs=dict(hotel=hotel_obj))
+
+      if els % 30 == 0:
+        request = {
+          'offset': f'{els}'
+        }
+
+        headers = {
+          'x-puid': '96449683-5e31-4b47-9440-e197195db75e',
+          'DNT': '1',
+          'authority': 'www.tripadvisor.com',
+          'accept': 'text/html, */*',
+          'x-requested-with': 'XMLHttpRequest',
+          'referer': f'{response.request.url}'
+        }
+
+        yield scrapy.FormRequest(response.request.url,
+                                 method= "POST",
+                                 callback=self.parse,
+                                 formdata=request,
+                                 headers=headers)
 
 
   def parse_details(self, response, hotel):
@@ -27,7 +52,7 @@ class TripadvisorSpider(scrapy.Spider):
     hotel['price'] = None
     
     rating = response.css('.bvcwU::text').extract()[0]
-    n_ratings = response.css('.btQSs::text').extract()[0]
+    n_ratings = response.css('.btQSs::text').extract()[0].split(' ')[0]
     hotel['rating'] = {'score': rating, 'n_ratings': n_ratings}
 
     try:
